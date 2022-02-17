a5a6e4103c2ab754d1b06015353cf78cf78216b7
Aleksander Klassanov <Aleksander.Klassanov@fourth.com>
2022-02-17 10:45:49 +0200

    Added more demos

---
+++ 06.Locking/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace _06.Locking
+{
+    class Program
+    {
+        private static List<int> Data = Enumerable.Range(0, 100).ToList();
+        private static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var thread = new Thread(Work);
+                thread.Start();
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Work()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+                lock (locker)
+                {
+                    Data.RemoveAt(Data.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ 07.Deadlock/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace _07.Deadlock
+{
+    class Program
+    {
+        private static object locker1 = new object();
+        private static object locker2 = new object();
+
+        static void Main(string[] args)
+        {
+            var th1 = new Thread(Method1);
+            var th2 = new Thread(Method2);
+
+            th1.Start();
+            th2.Start();
+        }
+
+
+        private static void Method1()
+        {
+            lock (locker1)
+            {
+                Console.WriteLine($" Thread Id: {Thread.CurrentThread.ManagedThreadId} locker1 locked");
+
+                Thread.Sleep(500);
+
+                lock (locker2)
+                {
+                    Console.WriteLine($" Thread Id: {Thread.CurrentThread.ManagedThreadId} locker2 locked");
+                }
+            }
+        }
+
+
+        private static void Method2()
+        {
+            lock (locker2)
+            {
+                Console.WriteLine($" Thread Id: {Thread.CurrentThread.ManagedThreadId} locker2 locked");
+
+                Thread.Sleep(500);
+
+                lock (locker1)
+                {
+                    Console.WriteLine($" Thread Id: {Thread.CurrentThread.ManagedThreadId} locker1 locked");
+                }
+            }
+        }
+    }
+}
