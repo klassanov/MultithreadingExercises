da87ddea6d646c2b0a5dec4a98856b5855e52acd
Aleksander Klassanov <Aleksander.Klassanov@fourth.com>
2022-02-25 20:05:44 +0200

    More examples added

---
+++ 14.Barrier/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace BarrierDemo
+{
+    class Program
+    {
+        //Barrier: Signals that a participant has reached the barrier and waits for all other participants to reach the barrier as well
+        private static readonly Barrier Barrier = new Barrier(3, (b)=>
+        {
+            //Called once on one of the threads but all are unblocked. Not really needed
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} unblocked");
+        });
+
+        private static readonly Random Random = new Random();
+
+        static void Main(string[] args)
+        {
+            new Thread(Speak).Start();
+            new Thread(Speak).Start();
+            new Thread(Speak).Start();
+        }
+
+
+        private static void Speak()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + " ");
+                Thread.Sleep(Random.Next(1000, 3000));
+                Barrier.SignalAndWait();
+            }
+        }
+    }
+}
---
+++ 15.ReaderWriterLock/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace _15.ReaderWriterLock
+{
+    class Program
+    {
+        //Many reads can occur in the same time, only 1 write can occur at the same time => optimization
+
+        //The write lock is all exclusive – blocks every single thread
+        //The read lock is does not block, if the write lock is free
+        //Correct way to do it with try/finally because we ensure that the release occurs event if we have an exception in the try block
+        private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim();
+
+        private static List<int> Items = new List<int>();
+        private static readonly Random Random = new Random();
+
+        static void Main(string[] args)
+        {
+            new Thread(Read).Start();
+            new Thread(Read).Start();
+            new Thread(Read).Start();
+
+            new Thread(Write).Start();
+            new Thread(Write).Start();
+        }
+
+
+        private static void Read()
+        {
+            while (true)
+            {
+                try
+                {
+                    Locker.EnterReadLock();
+
+                    foreach (var item in Items)
+                    {
+                        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} read {item}");
+                        Thread.Sleep(100);
+                    }
+                }
+
+                finally
+                {
+                    Locker.ExitReadLock();
+                }
+            }
+        }
+
+        private static void Write()
+        {
+            while (true)
+            {
+                int num = GetRandNum(100);
+
+                try
+                {
+                    Locker.EnterWriteLock();
+
+                    Items.Add(num);
+                }
+
+                finally
+                {
+                    Locker.ExitWriteLock();
+                }
+                
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} added {num}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static int GetRandNum(int max)
+        {
+            lock (Random)
+            {
+                return Random.Next(max);
+            }
+        }
+    }
+}
